4c54a92b6649435bff9acd6e26dedd3881a9b001
bob-young <583532940@qq.com>
2017-12-10 23:46:02 +0800

    clean branch

--- secSqlite3/sgxSqlite3/App/vfslib/stdio.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "Enclave_u.h"
-
-int ocall_remove(const char *pathname, int *perrno){
-	int ret = remove(pathname);
-    *perrno = errno;
-    return ret;
-}
--- secSqlite3/sgxSqlite3/sql/insert.sql
+++
@@ -1,3 +0,0 @@
-create table t1 (id int, name char(8));
-insert into t1 values (2,'scc');
-select * from t1;
